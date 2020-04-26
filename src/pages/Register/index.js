86b3aa65e9c64eac0c9c5c215e5e9503b1ce6745
artsantana <artsantana1457@hotmail.com>
2020-04-26 20:56:15 -0300

    Created Development

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function Register() {
   return (
-    <View />
+    <View>
+      <Text>Register Screen</Text>
+    </View>
   );
 }
